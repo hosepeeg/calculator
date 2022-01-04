124027352b5b2c80a1cb05520bf8e2b60f9a6f09
hosepeeg <hkourbouchian@gmail.com>
2022-01-04 14:28:57 -0500

    added all operational functions

---
+++ js/index.js
@@ -0,0 +1,48 @@
+const plusButton = 
+const minusButton =
+const multiplyButton =
+const divideButton =
+const modulasButton =
+const clearButton =
+const backSpaceButton =
+const oneButton = 
+const twoButton =
+const threeButton =
+const fourButton =
+const fiveButton =
+const sixButton =
+const sevenButton =
+const eightButton =
+const nineButton = 
+const zeroButton =
+const dotButton =
+const equalButton = 
+
+function add(a, b){
+    return a+b;
+}
+
+function subtract(a, b){
+    return a-b;
+}
+
+function multiply(a, b){
+    return a*b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+function modulas(a, b){
+    return a%b;
+}
+
+function backSpace(){
+
+}
+
+function clear(){
+
+}
+
